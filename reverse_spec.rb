b47f60d3f55309093d651dd683c76778bef12526
Kagheni <Kaghenimbale@gmail.com>
2023-06-01 15:30:12 +0200

    Add testing for the reverse methods

--- reverse_spec.rb
+++ reverse_spec.rb
@@ -7,5 +7,10 @@ describe Solver do
             solver = Solver.new
             expect(solver.reverse("Hello")).to eql('olleH')
         end
+
+        it "It should return a reverse string" do
+            solver = Solver.new
+            expect(solver.reverse("Chris")).to eql('sirhC')
+        end
     end
 end
